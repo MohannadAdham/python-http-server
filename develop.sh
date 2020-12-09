d0967f1236d2198c57ea98f81ad99f76a5d69977
Mohannad Adham <mohannad.adm@gmail.com>
2020-12-09 08:11:30 +0100

    Modified develop.sh to include starting and attaching the container

--- develop.sh
+++ develop.sh
@@ -10,6 +10,10 @@ docker create \
 --rm \
 --name=http_server \
 --publish-all \
---mount type=bind.source="$(pwd)/src",target=/home/http_server_user/src \
+--mount type=bind,source="$(pwd)/src",target=/home/http_server_user/src \
 --workdir /home/http_server_user/src \
 http_server:0.1
+
+docker start http_server
+docker port http_server
+docker attach http_server
