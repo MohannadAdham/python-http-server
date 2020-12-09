09e6b2c52f0725b26a8645f53b134e583ede7196
Mohannad Adham <mohannad.adm@gmail.com>
2020-12-09 03:25:40 +0100

    Added the commands for creating the docker container to develop.sh

---
+++ develop.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+docker build \
+--file=http_server.Dockerfile \
+--tag="http_server:0.1" \
+.
+
+docker create \
+--interactive \
+--tty \
+--rm \
+--name=http_server \
+--publish-all \
+--mount type=bind.source="$(pwd)/src",target=/home/http_server_user/src \
+--workdir /home/http_server_user/src \
+http_server:0.1
